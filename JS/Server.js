e7d269006c33f64c47614025c98d0b3906ea094f
ChelseaBui11 <141456665+ChelseaBui11@users.noreply.github.com>
2024-07-17 19:17:22 -0600

    Trying to get the layouts to work

--- JS/Server.js
+++ JS/Server.js
@@ -84,25 +84,18 @@ app.post('/user/:userId/layout', async (req, res) => {
     }
 });
 
-// Handle retrieving user layouts GET request
+// Fetch layouts by user ID
 app.get('/user/:userId/layouts', async (req, res) => {
+    const userId = req.params.userId;
     try {
-        const { userId } = req.params;
         const db = await connectToDB();
-        const usersCollection = db.collection('Users');
-
-        const user = await usersCollection.findOne({ _id: new ObjectId(userId) });
-
-        if (user) {
-            res.json(user.layouts);
-        } else {
-            res.status(404).json({ message: 'User not found' });
-        }
+        const layoutsCollection = db.collection('Layouts');
+        const layouts = await layoutsCollection.find({ userId }).toArray();
+        res.json(layouts);
     } catch (error) {
-        res.status(500).json({ message: 'Error fetching layouts' });
+        res.status(500).json({ message: 'Error fetching user layouts' });
     }
 });
-
 
 // Endpoint to get data from a specific collection
 app.get('/data/:collectionName', async (req, res) => {
