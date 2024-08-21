56e4968c3007d677f65b5a1718759e288eea4156
ChelseaBui11 <cbui@student.neumont.edu>
2024-08-21 09:37:16 -0600

    EC2 and Mongodb are now struggling...Being better safe than sorry and presenting on localhost

--- JS/Settings.js
+++ JS/Settings.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://18.232.160.182:3000/";
+const baseUrl = "http://localhost:3000/";
 
 document.addEventListener('DOMContentLoaded', function() {
     const formElement = document.getElementById('changePasswordForm');
--- JS/hummingbird.js
+++ JS/hummingbird.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://18.232.160.182:3000/";
+const baseUrl = "http://localhost:3000/";
 
 document.addEventListener('DOMContentLoaded', () => {
     const category = document.title.toLowerCase().includes('hummingbirds') ? 'hummingbirds' :
